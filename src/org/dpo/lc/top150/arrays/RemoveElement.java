c1b38bf98517fa79e9d0e0ed435fa3c26c093584
Dmytro Prokopenko <prokopenko.dmytro.i@gmail.com>
2025-06-28 21:26:28 +0300

    Remove Element

---
+++ src/org/dpo/lc/top150/arrays/RemoveElement.java
@@ -0,0 +1,24 @@
+package org.dpo.lc.top150.arrays;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] ints = {0, 1, 2, 2, 3, 0, 4, 2};
+        int res = removeElement(ints, 2);
+        System.out.println(res);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static int removeElement(int[] nums, int val) {
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+        return pointer;
+    }
+}
